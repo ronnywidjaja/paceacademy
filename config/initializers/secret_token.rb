d6b0ead4a536dacf4ff9c8657bdbb2dccec878bc
ferroconcrete <ferroconcrete@yedposterior.dreamhost.com>
2012-09-21 15:33:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pacedesign::Application.config.secret_token = '421f3cccb8c5217b502ff6a2ef2f5096025465a86e8a3bf88110e1670bcb751a6f541cf747a25b4122c68dc8a42afa0c72ad9a656560a8d1ee7e32c771860243'
